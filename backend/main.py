4b75de5adfbeeaba39ff43b1e24b5dbf8c429e0c
Andres <andresalo2011@gmail.com>
2022-12-22 12:18:55 +0100

    backend initial setup

--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from tensorflow.keras.models import load_model
-from tensorflow.keras.utils import get_file 
-from tensorflow.keras.utils import load_img 
-from tensorflow.keras.utils import img_to_array
 from tensorflow import expand_dims
-from tensorflow.nn import softmax
-from numpy import argmax
-from numpy import max
-from numpy import array
-from json import dumps
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import get_file, load_img, img_to_array
 from uvicorn import run
+from io import BytesIO
+from PIL import Image
 import os
 
 app = FastAPI()
 
-origins = ["*"]
-methods = ["*"]
-headers = ["*"]
-
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins = origins,
+    allow_origins = ["*"],
     allow_credentials = True,
-    allow_methods = methods,
-    allow_headers = headers    
+    allow_methods = ["*"],
+    allow_headers = ["*"]    
 )
 
 model_dir = "ml/model/EfficientNetB4.h5"
 model = load_model(model_dir)
 
-@app.get("/")
-async def root():
-    label_path = "ml/examples/1.jpg"
-    img = load_img(
-        label_path
-    )
-    img_array = img_to_array(img)
+def read_image_file(file) -> Image.Image:
+    image = Image.open(BytesIO(file))
+    return image
+
+@app.post("/")
+async def root(image: UploadFile):
+    if image.filename == "":
+        return {"message": "No image provided"}
+    
+    loaded_image = read_image_file(await image.read())
+    img_array = img_to_array(loaded_image)
     img_array = expand_dims(img_array, 0)
     probabilities = model.predict(img_array)
     probabilities = probabilities[0].tolist()
@@ -49,10 +44,9 @@ async def root():
         "BKL": probabilities[4], 
         "DF": probabilities[5], 
         "VASC":probabilities[6], 
-        "SCC":probabilities[7]  
+        "SCC":probabilities[7]
     }
-
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
-	run(app, host='0.0.0.0', port=port)
+	run(app, host='127.0.0.1', port=port)
